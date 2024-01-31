c4bc870f2afceb97822ee852102a8f81c4858ba5
Shubham Mahobia <mahobiashubham4@gmail.com>
2024-01-31 17:16:55 +0530

    cron job to remove unverified account

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 var express = require('express');
+const schedule = require('node-schedule');
+const { removingUnverifiedEMails } = require('./controller/signInController');
 var app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
@@ -12,3 +14,8 @@ app.use('/api',router);
 
 //Connection
 mongoose.connect('mongodb://127.0.0.1:27017/ride_eye_dev').then(() => console.log("Database connected")).catch(err => console.log("Mongo Connection Error "+ err));
+
+//Cron Job to remove unverified Emails -
+schedule.scheduleJob('0 */12 * * *', () => {
+    removingUnverifiedEMails();
+});
